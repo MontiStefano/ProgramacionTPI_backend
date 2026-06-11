3021431910d251ba4f09225a81537150017593f4
joacomoron <jo4co11m@gmail.com>
2026-06-11 19:49:55 -0300

    actualizados controllers de turnos con campos correctos

--- src/routes/servicios.routes.js
+++ src/routes/servicios.routes.js
@@ -4,7 +4,7 @@ import { getAllServicios, getServicioById, createServicio, updateServicio, delet
 import { verifyToken } from "../Middleware/VerifyToken.js";
 
 const router = Router();
-router.get("/servicios", verifyToken, getAllServicios);
+router.get("/servicios", getAllServicios);
 router.get("/servicios/:id", verifyToken, getServicioById);
 router.post("/servicios", verifyToken, createServicio);
 router.put("/servicios/:id", verifyToken, updateServicio);
